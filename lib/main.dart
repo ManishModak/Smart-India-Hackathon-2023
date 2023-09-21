0d0c8977a3ee7b1537991f3cd3692ca6af10bfa5
Sahil Sunil Phadtare <sahil.sunil.phadtare.33@gmail.com>
2023-09-21 23:06:11 +0530

    Updated the map

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       initialRoute: '/home',
       routes: {
-        '/home': (context) => const Home(),
+        '/home': (context) => const HomePage(),
         '/search': (context) => const Search(),
         '/chat': (context) => const Chat(),
         '/agencydetail':(context) => const AgencyDetail(),
--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,18 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomeState extends State<Home> {
+class _HomePageState extends State<HomePage> {
+  late LatLng  _userLocation; // Default location
+  GeolocatorPlatform geolocator = GeolocatorPlatform.instance;
+  int _selectedIndex = 0; // Index for the selected bottom navigation bar item
+
+  @override
+  void initState() {
+    super.initState();
+    _initUserLocation();
+  }
+
+  Future<void> _initUserLocation() async {
+    Position position = await geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    setState(() {
+      _userLocation = LatLng(position.latitude, position.longitude);
+    });
+  }
+
+  // Function to handle item selection in the bottom navigation bar
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FlutterMap(
+              options: MapOptions(
+                center: _userLocation,
+                zoom: 13.0,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: const ['a', 'b', 'c'],
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      width: 30.0,
+                      height: 30.0,
+                      point: _userLocation,
+                      builder: (ctx) => const Icon(
+                        Icons.location_pin,
+                        color: Colors.blue,
+                        size: 30.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+            ],
+            currentIndex: _selectedIndex, // Current selected index
+            onTap: _onItemTapped, // Function to handle item tap
+          ),
+        ],
       ),
     );
   }
