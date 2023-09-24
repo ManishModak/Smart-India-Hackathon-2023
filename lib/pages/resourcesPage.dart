ed1ae73019bab28773c20f2ce39b1d12e46ea4f5
manish <manishmodak90@gmail.com>
2023-09-25 00:22:44 +0530

    Added Resource Page (Beta)

--- lib/pages/resourcesPage.dart
+++ lib/pages/resourcesPage.dart
@@ -1,99 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:smart_india_hackathon/services/agencyServices.dart';
 
 class ResourcesPage extends StatefulWidget {
-  const ResourcesPage({super.key});
+  final String agencyId;
+
+  const ResourcesPage({Key? key, required this.agencyId}) : super(key: key);
 
   @override
   State<ResourcesPage> createState() => _ResourcesPageState();
 }
 
 class _ResourcesPageState extends State<ResourcesPage> {
+  AgencyDatabase agencyDatabase = AgencyDatabase();
+  Map<String, dynamic> data = {};
 
-  TextEditingController agencyName = TextEditingController();
-  TextEditingController location = TextEditingController();
+  TextEditingController name = TextEditingController();
+  TextEditingController address = TextEditingController();
   TextEditingController phone = TextEditingController();
-  TextEditingController email = TextEditingController();
   TextEditingController about = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    getInfo();
+  }
+
+  Future<void> getInfo() async {
+    data = await agencyDatabase.getAgency(id: widget.agencyId);
+
+    // Update the text controllers with the fetched data
+    name.text = data["name"];
+    address.text = data["address"];
+    phone.text = data["phone"];
+    about.text = data["description"];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Agency Details'),
+        title: const Center(child: Text('Resources')),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Center(
-              child: Text(
-                "agencyName.text",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+          children: [
+            // Agency Name
+            const Text(
+              "Agency Name:",
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 8),
-            Center(
-              child: Text(
-                'Location: $location',
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
-              ),
+            Text(name.text),
+            const SizedBox(height: 16.0),
+
+            // Agency No
+            const Text(
+              "Agency No:",
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 38),
-            const Center(
-              child: Text(
-                'Contact Information:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+            Text(phone.text),
+            const SizedBox(height: 16.0),
+
+            // Agency Address
+            const Text(
+              "Agency Address:",
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 8),
-            Center(
-              child: Text(
-                'Phone: $phone',
-                style: const TextStyle(
-                  fontSize: 16,
-                ),
-              ),
+            Text(address.text),
+            const SizedBox(height: 16.0),
+
+            // Agency Description
+            const Text(
+              "Agency Description:",
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            Center(
-              child: Text(
-                'Email: $email',
-                style: const TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ),
-            const SizedBox(height: 44),
-            const Center(
-              child: Text(
-                'About the Agency',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            const SizedBox(height: 8),
-            const Center(
-              child: Text(
-                'about.text',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ),
+            Text(about.text),
           ],
         ),
       ),
     );
   }
 }
-
